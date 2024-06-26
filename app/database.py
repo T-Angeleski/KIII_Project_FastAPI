d181e9b16255f6f0a5f62e61cc3c423ea55b5eca
Teodor Angeleski <teodorangeleski@gmail.com>
2024-06-26 13:37:18 +0200

    recommit

--- app/database.py
+++ app/database.py
@@ -3,31 +3,30 @@ import os
 from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker
 
-POSTGRES_URL = os.getenv("POSTGRES_URL")
+# POSTGRES_URL = os.getenv("POSTGRES_URL")
 POSTGRES_DATABASE = os.getenv("POSTGRES_DATABASE")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
 
-if POSTGRES_URL is None:
-	raise RuntimeError("Env POSTGRES_URL is not set")
+# url = URL.create(
+# 	drivername="postgresql",
+# 	username=POSTGRES_USER,
+# 	password=POSTGRES_PASSWORD,
+# 	host=POSTGRES_HOST,
+# 	database=POSTGRES_DATABASE,
+# 	port=POSTGRES_PORT
+# )
+SQLALCHEMY_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DATABASE}"
 
-if POSTGRES_DATABASE is None:
-	raise RuntimeError("Env POSTGRES_DATABASE is not set")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
-if POSTGRES_USER is None:
-	raise RuntimeError("Env POSTGRES_USER is not set")
+# Testing connection
+try:
+	with engine.connect() as connection:
+		print("Connection to database successful!")
+except Exception as e:
+	print(f"Error connecting: {str(e)}")
 
-if POSTGRES_PASSWORD is None:
-	raise RuntimeError("Env POSTGRES_PASSWORD is not set")
-
-url = URL.create(
-	drivername="postgresql",
-	username=POSTGRES_USER,
-	password=POSTGRES_PASSWORD,
-	host="db",
-	database=POSTGRES_DATABASE,
-	port=5432
-)
-
-engine = create_engine(url)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
